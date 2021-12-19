cb4690ab41226c3847c69ec222092a1b0d304b40
ilmar7786 <ilmar7786@yandex.ru>
2021-12-19 13:43:51 +0300

    feat(userOverview/frontend): add rollback

--- client/src/components/pages/UserOverview/index.js
+++ client/src/components/pages/UserOverview/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ListUsers from "./listUsers";
+import RollBack from "../../rollBack";
 
 const UserOverview = () => {
     return (
         <>
+            <RollBack text={'Список пользователей'}/>
             <ListUsers/>
         </>
     );
