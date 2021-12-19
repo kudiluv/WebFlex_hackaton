6a958abea56de856e90f3e6cb86bd460ddf07417
Pavel <pavlikberezhnoy@mail.ru>
2021-12-19 16:14:55 +0300

    dev: prod

--- client/src/components/filepreview/FilePreview.jsx
+++ client/src/components/filepreview/FilePreview.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import docPreview from './../media/images/word.png';
+import videoPreview from './../media/images/video-icon.png';
 import style from './style/style.module.css';
 
 const FilePreview = (props) => {
     return (
         <a href={`${process.env.REACT_APP_LOCALHOST}/${props.path}`} target="_blank">
             <div className={style.lecture}>
-                <div className={style.preview}><img src={props.type === "word" ? docPreview : ''} alt='' /></div>
+                <div className={style.preview}><img src={props.type === "word" ? docPreview : videoPreview} alt='' /></div>
                 <div className={style.date}>{props.updatedAt}</div>
             </div>
         </a>
