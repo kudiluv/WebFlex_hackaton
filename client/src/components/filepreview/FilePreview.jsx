58e01558faee9af7a9c81a65dec8fe42f7baccac
Pavel <pavlikberezhnoy@mail.ru>
2021-12-19 16:04:22 +0300

    dev: prod

--- client/src/components/filepreview/FilePreview.jsx
+++ client/src/components/filepreview/FilePreview.jsx
@@ -4,7 +4,7 @@ import style from './style/style.module.css';
 
 const FilePreview = (props) => {
     return (
-        <a href={`http://localhost:80/${props.path}`} target="_blank">
+        <a href={`${process.env.REACT_APP_LOCALHOST}/${props.path}`} target="_blank">
             <div className={style.lecture}>
                 <div className={style.preview}><img src={props.type === "word" ? docPreview : ''} alt='' /></div>
                 <div className={style.date}>{props.updatedAt}</div>
