633d78377594e08d29f993609ba8e35ad9688825
Pavel <pavlikberezhnoy@mail.ru>
2021-12-19 15:56:18 +0300

    dev: prod

--- client/src/components/student/pages/search/SearchResult.jsx
+++ client/src/components/student/pages/search/SearchResult.jsx
@@ -4,7 +4,7 @@ import styles from './styles/search.module.css';
 
 const SearchResult = (props) => {
     return (
-        <Link to={props.id}>
+        <Link to={`/lecture/${props.item.id}`}>
             <div className={styles.search__result}>
                 <h2 className={styles.result__title}>{props.item.name}</h2>
                 <p className={styles.result__shortDesc}>{props.item.description}</p>
